3229e51b7f72bae80162f9fdc671c50e7fb21e72
yuioProd <129677179+yuioProd@users.noreply.github.com>
2023-04-02 22:35:47 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -14,7 +14,7 @@ addLayer("p", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new ExpantaNum(1)
+        mult = new ExpantaNum(player[this.layer].points)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
