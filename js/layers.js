504c3bcce8f99ae886519547605109689635d3d1
yuioProd <129677179+yuioProd@users.noreply.github.com>
2023-04-05 18:45:51 +0700

    test

--- js/layers.js
+++ js/layers.js
@@ -8,27 +8,112 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new ExpantaNum(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "test points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     effect() {
-        return new ExpantaNum.pow(2, player[this.layer].points);
+        mult = new ExpantaNum(player[this.layer].points).add(1)
+        if (hasUpgrade(this.layer, "11")) mult = mult.pow(upgradeEffect(this.layer, "11"))
+        return mult
     },
-    effectDescription(){
-        return "which x" + format(tmp[this.layer].effect)
+    effectDescription() {
+        return `which boost points gain by <h2 style="color: ${this.color}; text-shadow: 0px 0px 10px ${this.color}">${formatWhole(this.effect())}</h2>`
     },
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new ExpantaNum(1)
+        if (hasUpgrade(this.layer, "12")) mult = mult.times(upgradeEffect(this.layer, "12"))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new ExpantaNum(1)
+        exp = new ExpantaNum(1)
+        if (hasUpgrade(this.layer, "13")) exp = exp.add(upgradeEffect(this.layer, "13"))
+        exp = exp.add(tmp[this.layer].buyables["11"].effect)
+        return exp
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "p", description: "P: Reset for test points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    passiveGeneration() {
+        /*if (hasUpgrade(this.layer, "14"))*/ return getResetGain(this.layer, "static")
+    },
+    upgrades: {
+        11: {
+            title: "Test",
+            description: "Squaer the test points effect.",
+            cost: new ExpantaNum(10),
+            effect() {
+                return new ExpantaNum(2);
+            },
+        },
+        12: {
+            title: "Test2",
+            description() { 
+                return `Points multiply base test points gain.
+                
+                Currently: ${format(upgradeEffect(this.layer, "12"))}x`
+            },
+            cost: new ExpantaNum(1e10),
+            effect() {
+                return new ExpantaNum(player.points).add(10).logBase(10);
+            },
+        },
+        13: {
+            title: "Test3",
+            description: "Increase test points gain exponent by 0.500.",
+            cost: new ExpantaNum("1.798e308"),
+            effect() {
+                return new ExpantaNum(0.5);
+            },
+        },
+        14: {
+            title: "Test4",
+            description: "Gain 100% of test points gain per second and unlock a buyable.",
+            cost: new ExpantaNum("1e20000000"),
+            effect() {
+                return new ExpantaNum(0.5);
+            },
+        },
+        15: {
+            title: "Test5",
+            description: "???",
+            cost: new ExpantaNum("e1.798e308"),
+            effect() {
+                return new ExpantaNum(0.5);
+            },
+        },
+    },
+    buyables: {
+        11: {
+            title: "Testing test",
+            cost(x) { 
+                let cost = ExpantaNum.pow("1e20000020", x.add(1).tetr(2).times(0.0000001).add(1))
+                return cost.floor()
+            },
+            effect(x) {
+                let eff = ExpantaNum(0).add(x).times(0.1)
+                return eff;
+            },
+            display() {
+                let data = tmp[this.layer].buyables[this.id]
+                return "Cost: " + format(data.cost) + " test points\n\
+                Amount: " + player[this.layer].buyables[this.id] + "\n\
+                Adds +" + format(data.effect) + " to test points gain exponent"
+            },
+            unlocked() { return hasUpgrade(this.layer, "14") }, 
+            canAfford() {
+                return player[this.layer].points.gte(tmp[this.layer].buyables[this.id].cost)
+            },
+            buyMax() {}, // You'll have to handle this yourself if you want
+            buy() { 
+                cost = tmp[this.layer].buyables[this.id].cost
+                player[this.layer].points = player[this.layer].points.sub(cost)	
+                player[this.layer].buyables[this.id] = player[this.layer].buyables[this.id].add(1)
+                player[this.layer].spentOnBuyables = player[this.layer].spentOnBuyables.add(cost) // This is a built-in system that you can use for respeccing but it only works with a single ExpantaNum value
+            },
+        },
+    },
 })
--- js/utils/NumberFormating.js
+++ js/utils/NumberFormating.js
@@ -28,30 +28,52 @@ function sumValues(x) {
     return x.reduce((a, b) => ExpantaNum.add(a, b))
 }
 
-function format(decimal, precision = 2, small=false) {
+function format(decimal, precision = 3, small=false) {
+    precDef = 3
     small = small || modInfo.allowSmall
     decimal = new ExpantaNum(decimal)
-    let fmt = decimal.toString(precision)
-    if(decimal.gte(1000)&&decimal.lt("10^^5")){
-      let powers = fmt.split("e")
-      for (let i in powers){
-        let x=Number(powers[i])
-        if(Number.isNaN(x)||x==Infinity){}
-        else if(Number(powers[i])>(1000)){
-          let st = powers[i]
-          let s=st.length
-          if(s==4){st=st[0]+","+st.substr(1,3)}
-          if(s==5){st=st.substr(0,2)+","+st.substr(2,3)}
-          if(s==6){st=st.substr(0,3)+","+st.substr(3,3)}
-          if(s==7){st=st.substr(0,1)+","+st.substr(1,3)+","+st.substr(4,3)}
-          if(s==8){st=st.substr(0,2)+","+st.substr(2,3)+","+st.substr(5,3)}
-          if(s==9){st=st.substr(0,3)+","+st.substr(3,3)+","+st.substr(6,3)}
-          powers[i]=st
+    let fmt = decimal.toString()
+    if (decimal.gte("10^^5")) return decimal.toHyperE()
+    else if(decimal.gte(0.001)) {
+        let powers = fmt.split("e")
+        for (let i in powers){
+            if (Number(powers[i]) >= 1000000000) {
+                a = Math.floor(Math.log10(Number(powers[i])))
+                powers.push(a.toString())
+                if (i>0) powers[i-1] = ""
+                b = Number(powers[i]) / (10**a)
+                powers[i] = b.toString()
+            }
+            else if (Number(powers[i]) >= 1000000 && i > 0) {
+                powers[i-1] = Math.floor(Number(powers[i-1]))
+            }
+            if (Number(powers[i]) >= 1000) powers[i] = powers[i].split(".")[0]
+            if (powers[i].split(".").length > 1 && (precision > 0 || powers.length > 1) && Number(powers[i]) < 1000) {
+                if (powers[i].split(".")[1].length > precDef) {
+                    let f = powers[i].split(".")
+                    powers[i] = f[0] + "." + f[1].substring(0, precDef)
+                }
+                else if (powers[i].split(".")[1].length < precDef) powers[i] = powers[i] + "0".repeat(precDef - powers[i].split(".")[1].length)
+            } 
+            else if (powers[i].split(".").length == 1 && (precision > 0 || powers.length > 1) && Number(powers[i]) < 1000 && powers[i].length > 0 && (i != powers.length - 1 || i == 0)) powers[i] = powers[i] + ".000"
+            let x = Number(powers[i])
+            if (Number.isNaN(x) || x == Infinity) {}
+            else if (Number(powers[i]) >= 1000) {
+                let st = powers[i]
+                let s = st.length
+                if (s == 4) st = st[0] + "," + st.substr(1, 3)
+                if (s == 5) st = st.substr(0, 2) + "," + st.substr(2, 3)
+                if (s == 6) st = st.substr(0, 3) + "," + st.substr(3, 3)
+                if (s == 7) st = st.substr(0, 1) + "," + st.substr(1, 3) + "," + st.substr(4, 3)
+                if (s == 8) st = st.substr(0, 2) + "," + st.substr(2, 3) + "," + st.substr(5, 3)
+                if (s == 9) st = st.substr(0, 3) + "," + st.substr(3, 3) + "," + st.substr(6, 3)
+                powers[i] = st
+            }
         }
-      }
-      fmt=powers.join("e")
-      return fmt}
-    else if(precision>0){
+        fmt = powers.join("e")
+        return fmt
+    }
+    else if (precision>0) {
       if(fmt.split(".").length==1){fmt=fmt+".00"}
       else if(fmt.split(".")[1].length==1){fmt=fmt+"0"}
     }
